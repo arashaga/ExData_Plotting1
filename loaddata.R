25b07510d4d37cbba98960c133f259686579d536
Arash Mosharraf <arashaga@gmail.com>
2016-04-22 10:15:20 -0500

    load data fix

--- loaddata.R
+++ loaddata.R
@@ -2,7 +2,8 @@
 
 
 getData <-function(){
-  uci01 <- read.delim("/Users/arashaga/Downloads/household_power_consumption.txt",sep=";")
+  ## Fix the directory to point to the data ok?
+  uci01 <- read.delim("household_power_consumption.txt",sep=";")
   uci01$Date <-as.Date(uci01$Date,"%d/%m/%Y")
   data <-subset(uci01, Date >="2007-01-01" & Date <="2007-02-01" )
   rm(uci01)
